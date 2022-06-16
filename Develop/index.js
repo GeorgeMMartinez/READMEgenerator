242efefb5a2959bd07aa214637b084330226b5cf
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-06-16 00:50:00 -0700

    migrate files

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util')
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -27,37 +28,52 @@ const questions = [
   },
   {
     type: 'input',
-    message: 'Explain the steps on how to install your project for the Installation section.',
+    message: 'Explain the steps needed to install your project.',
     name: 'installation',
   },
   {
     type: 'input',
-    message: "Provide instructions and examples of your project in use for the Usage section.",
-    name: 'usage'
+    message: "Provide instructions and examples of your project in use.",
+    name: 'usage',
   },
   {
     type: 'input',
     message: "Provide guidelines on how others can contribute to your project.",
-    name: 'contributing'
+    name: 'contributing',
   },
   {
     type: 'input',
-    message: "Provide any tests written for your application and examples on how to run them.",
-    name: 'tests'
+    message: "How can you test your project?",
+    name: 'tests',
   },
   {
     type: 'list',
     message: "Choose a license for your project.",
-    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-    name: 'license'
+    choices: ['None', 'GNU GPL 3.0', 'Apache License 2.0', 'MIT License'],
+    name: 'license',
   }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    } else {
+    console.log("Success! Your READ.ME has been generated.")
+    }
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(function (data) {
+      console.log(data);
+      var fileContent = generateMarkdown(data);
+      writeToFile(fileContent)
+    });
+}
 
 // Function call to initialize app
 init();
